b739b8ba07bca6e1d883ce904f540e8c1cae7743
Jomael Gemota <jomaelgemota@gmail.com>
2025-07-04 03:10:20 +0800

    update nextjs config

--- portal/next.config.ts
+++ portal/next.config.ts
@@ -1,10 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   eslint: {
     ignoreDuringBuilds: true
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
